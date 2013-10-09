b29d10c3cf8c9f98916a3553d6ed8e800452b5b5
Dan Farrelly <daniel.j.farrelly@gmail.com>
2013-10-09 12:17:30 -0400

    Switched to new hbs module

--- app/index.js
+++ app/index.js
@@ -1,5 +1,3 @@
 var train = require('express-train');
 
-console.log("DIRNAME index.js - ", __dirname);
-
 module.exports = train(__dirname);
--- app/lib/views.js
+++ app/lib/views.js
@@ -1,28 +1,21 @@
-var hbs = require('express-hbs')
-  , path = require('path')
-  , fs = require('fs');
+var hbs = require('hbs') //hbs = require('express-hbs')
+  , path = require('path');
 
 module.exports = function (app) {
 
-    //set up view engine
+    // Using hbs
     app.set('view engine', 'hbs');
 
-    console.log('Partials path', path.join(__dirname, '../views/partials'));
+    hbs.registerPartials(path.join(__dirname, '../views/partials'));
 
-    // testing if there is anything in the partials folder
-    fs.readdir(path.join(__dirname, '../views/partials'), function(err, files){
-      if (err) console.log(err);
 
-      console.log('FILES FOUND', files);
-    });
-
-    app.set('views', path.join(__dirname, '../views'));
-
-    app.engine('hbs', hbs.express3({
-        partialsDir: path.join(__dirname, '../views/partials')
-    }));
+    // When using express-hbs
+    // app.set('view engine', 'hbs');
+    // app.set('views', path.join(__dirname, '../views'));
+    // app.engine('hbs', hbs.express3({
+    //     partialsDir: path.join(__dirname, '../views/partials')
+    // }));
 
     // Static locals
-    app.locals({
-    });
+    app.locals({});
 };
