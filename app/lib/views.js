beecab8fe45a4e1fa243c8e7baacb43b1bfe70f2
Dan Farrelly <daniel.j.farrelly@gmail.com>
2013-10-09 11:52:18 -0400

    Checking files

--- app/lib/views.js
+++ app/lib/views.js
@@ -1,15 +1,25 @@
-var hbs = require('express-hbs'),
-    path = require('path');
+var hbs = require('express-hbs')
+  , path = require('path')
+  , fs = require('fs');
 
 module.exports = function (app) {
 
     //set up view engine
     app.set('view engine', 'hbs');
 
-    console.log("Dirname:", __dirname, 'filename: ', __filename, "Partials path", path.join(__dirname, "../views/partials"));
+    console.log('Partials path', path.join(__dirname, '../views/partials'));
+
+    // testing if there is anything in the partials folder
+    fs.readdir(path.join(__dirname, '../views/partials'), function(err, files){
+      if (err) console.log(err);
+
+      console.log('FILES FOUND', files);
+    });
+
+    app.set('views', path.join(__dirname, '../views'));
 
     app.engine('hbs', hbs.express3({
-        partialsDir:path.join(__dirname, "../views/partials")
+        partialsDir: path.join(__dirname, '../views/partials')
     }));
 
     // Static locals
