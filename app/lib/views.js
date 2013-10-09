e782ef1a2bc077e8ce7a72427794ed71f7f6bc85
Dan Farrelly <daniel.j.farrelly@gmail.com>
2013-10-09 10:45:30 -0400

    Debug handlebars template partials

--- app/lib/views.js
+++ app/lib/views.js
@@ -6,6 +6,8 @@ module.exports = function (app) {
     //set up view engine
     app.set('view engine', 'hbs');
 
+    console.log("Partials path", path.join(__dirname, "../views/partials"));
+
     app.engine('hbs', hbs.express3({
         partialsDir:path.join(__dirname, "../views/partials")
     }));
